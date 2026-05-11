08af52c3b1bb1c46e8854e9af27f5bda78db1ad0
abayy177 <akbar.ramdani121@smk.belajar.id>
2026-05-11 13:55:18 +0700

    sudah menyelasaikan migration dan model

---
+++ app/Models/Class_student.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Class_student extends Model
+{
+    protected $fillable = [
+        'class_student_name'
+    ];
+}
